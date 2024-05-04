3196cfa0f42f68067f8db5e49b1a4a3efe99e3e9
Kremilly <contact@kremilly.com>
2024-05-03 21:26:31 -0300

    feat: separate Paimon flags into a new file, exclusively for this purpose

---
+++ src/args_cli.rs
@@ -0,0 +1,45 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+pub struct Flags {
+    #[arg(long)]
+    /// The paimon file to create a new Monlib list
+    pub file: Option<String>,
+
+    #[arg(long)]
+    /// Title of a new Monlib list
+    pub title: Option<String>,
+
+    #[arg(long)]
+    /// Privacy of a new Monlib list
+    pub privacy: Option<String>,
+
+    #[arg(short, long)]
+    /// Run a Monlib list or execute a specific list
+    pub run: Option<String>,
+
+    #[arg(long)]
+    /// No ignore any pdf files
+    pub noignore: bool,
+
+    #[arg(long)]
+    /// Disable the comments and !debug macro
+    pub no_comments: bool,
+
+    #[arg(long)]
+    /// Your Kindle email for send the ebooks for your account
+    pub kindle: Option<String>,
+
+    #[arg(long)]
+    /// Your Paimon settings
+    pub options: Option<String>,
+
+    #[arg(long)]
+    /// Inspect the pdf files at library selected
+    pub inspect: bool,
+
+    #[arg(short, long)]
+    /// Publish a new library in your Monlib account
+    pub publish: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,66 +2,23 @@ mod api;
 mod cmd;
 mod utils;
 mod configs;
+mod args_cli;
 
 use clap::Parser;
 use std::error::Error;
 
-use colored::*;
-use figlet_rs::FIGfont;
+use crate::args_cli::Flags;
 
 use crate::utils::misc::Misc;
 
 use crate::configs::env::Env;
-use crate::configs::global::Global;
 
 use crate::cmd::bootstrap::Paimon;
 
-use crate::api::api_get_list::ApiGetList;
-use crate::api::api_publish_list::ApiPublishList;
-
-#[derive(Parser)]
-#[clap(author, version, about)]
-struct Args{
-    #[arg(long)]
-    /// The paimon file to create a new Monlib list
-    file: Option<String>,
-
-    #[arg(long)]
-    /// Title of a new Monlib list
-    title: Option<String>,
-
-    #[arg(long)]
-    /// Privacy of a new Monlib list
-    privacy: Option<String>,
-
-    #[arg(short, long)]
-    /// Run a Monlib list or execute a specific list
-    run: Option<String>,
-
-    #[arg(long)]
-    /// No ignore any pdf files
-    noignore: bool,
-
-    #[arg(long)]
-    /// Disable the comments and !debug macro
-    no_comments: bool,
-
-    #[arg(long)]
-    /// Your Kindle email for send the ebooks for your account
-    kindle: Option<String>,
-
-    #[arg(long)]
-    /// Your Paimon settings
-    options: Option<String>,
-
-    #[arg(long)]
-    /// Inspect the pdf files at library selected
-    inspect: bool,
-
-    #[arg(short, long)]
-    /// Publish a new library in your Monlib account
-    publish: bool,
-}
+use crate::api::{
+    api_get_list::ApiGetList, 
+    api_publish_list::ApiPublishList
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -69,7 +26,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         eprintln!("Error: {}", err);
     }
 
-    let args_parser: Args = Args::parse();
+    let args_parser: Flags = Flags::parse();
     let run = args_parser.run.as_deref().unwrap_or_default();
 
     if !run.is_empty() {
