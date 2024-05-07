7f54151e036adab8063dba67e21b0742bd43cbb3
Kremilly <contact@kremilly.com>
2024-05-07 02:21:47 -0300

    feat: rename file from templates/readme.html to templates/render.html

--- src/render/render_env.rs
+++ src/render/render_env.rs
@@ -8,7 +8,7 @@ pub struct RenderMarkdownEnv;
 impl RenderMarkdownEnv {
 
     pub const README_APP_NAME: &'static str = Global::APP_NAME;
-    pub const README_TEMPLATE_FILE: &'static str = "templates/readme.html";
+    pub const README_TEMPLATE_FILE: &'static str = "templates/render.html";
     pub const README_CSS_BASE_FILE: &'static str = "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.5.1/github-markdown-dark.min.css";
 
     pub const README_FOLDER: Lazy<PathBuf> = Lazy::new(|| {
