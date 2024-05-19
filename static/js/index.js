1f12946c2e76ca9c6f095070ecebe5d51ff04a4b
Kremilly <contact@kremilly.com>
2024-05-19 04:12:55 -0300

    feat: added a header on readme template file

--- static/js/index.js
+++ static/js/index.js
@@ -2,5 +2,8 @@ window.onload = e => {
 
     Citations.init();
     DocsSources.init();
+
+    let pageTitle = document.title.split(':');
+    document.getElementById('header-label').textContent = `${ pageTitle[1] }: ${ pageTitle[2] }`;
     
 };
