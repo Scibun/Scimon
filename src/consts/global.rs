95baceedc19b767e6010087631eab00f239329e9
Kremilly <contact@kremilly.com>
2024-06-03 00:05:54 -0300

    refactor: update Cargo.toml and global.rs with dynamic values

--- src/consts/global.rs
+++ src/consts/global.rs
@@ -3,9 +3,9 @@ pub struct Global;
 impl Global {
 
     pub const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+    pub const APP_AUTHOR: &'static str = env!("CARGO_PKG_AUTHORS");
     pub const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
-    pub const APP_AUTHOR: &'static str = "@Kremilly";
-    pub const APP_HOMEPAGE: &'static str = "https://github.com/Kremilly/Paimon";
+    pub const APP_HOMEPAGE: &'static str = env!("CARGO_PKG_HOMEPAGE");
 
     pub const ENV_URL: &'static str = "https://pastebin.com/raw/wZGaNtsL";
     pub const SETTING_URL: &'static str = "https://pastebin.com/raw/jVdvKhWg";
