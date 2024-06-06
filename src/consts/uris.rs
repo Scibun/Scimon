2cd9d6d891ae30addfb68a4c4c34418ab7adcee8
Kremilly <contact@kremilly.com>
2024-06-06 02:59:44 -0300

    refactor: update package name, UI rendering, and mode values

--- src/consts/uris.rs
+++ src/consts/uris.rs
@@ -12,8 +12,8 @@ impl Uris {
 
     // Ravenlib
     pub const RAVENLIB_API_REQUEST: &'static str = "http://localhost/Ravenlib/api/";
-    pub const SCIHUB_API_REQUEST: &'static str = "https://api.kremilly.com/scihub?paper=";
-    pub const PAIMON_SCRAPE_API_REQUEST: &'static str = "https://api.kremilly.com/scrape?url=";
+    pub const SCIHUB_API_REQUEST: &'static str = "https://addons.scibun.com/scihub?paper=";
+    pub const PAIMON_SCRAPE_API_REQUEST: &'static str = "https://addons.scibun.com/scrape?url=";
 
     // Third-parties
     pub const WIKIPEDIA_API_REQUEST_PDF: &'static str = "https://en.wikipedia.org/api/rest_v1/page/pdf/";
