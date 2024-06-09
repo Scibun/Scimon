e841d843fdc58e3b67f221d42bff40cfdea821f5
Kremilly <contact@kremilly.com>
2024-06-09 09:41:27 -0300

    refactor: update README template links to use new domain

--- src/consts/prime_down.rs
+++ src/consts/prime_down.rs
@@ -2,7 +2,7 @@ pub struct PrimeDownEnv;
 
 impl PrimeDownEnv {
 
-    pub const README_TEMPLATE_LINK: &'static str = "https://read-me-template.vercel.app";
-    pub const README_TEMPLATE_BUNDLE_LINK: &'static str = "https://read-me-template.vercel.app/static/dist/";
+    pub const README_TEMPLATE_LINK: &'static str = "https://template.scibun.com/";
+    pub const README_TEMPLATE_BUNDLE_LINK: &'static str = "https://template.scibun.com/static/dist/";
 
 }
