373de505cdbb0f84258ad446b2965b11edb1d63d
Kremilly <contact@kremilly.com>
2024-06-09 15:05:35 -0300

    refactor: update ENV_URL and SETTING_URL to use new repository URLs

--- src/consts/global.rs
+++ src/consts/global.rs
@@ -7,7 +7,7 @@ impl Global {
     pub const APP_VERSION: &'static str = env!("CARGO_PKG_VERSION");
     pub const APP_HOMEPAGE: &'static str = env!("CARGO_PKG_HOMEPAGE");
 
-    pub const ENV_URL: &'static str = "https://pastebin.com/raw/wZGaNtsL";
+    pub const ENV_URL: &'static str = "https://raw.githubusercontent.com/Scibun/Scimon/main/.env.example";
     pub const SETTING_URL: &'static str = "https://raw.githubusercontent.com/Scibun/Scimon/main/scimon.yml";
 
     pub const PB_STYLE: &'static str = "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})";
