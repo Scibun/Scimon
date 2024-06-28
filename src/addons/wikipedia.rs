0f49ac700dbce4c2a99e54bff4901f88bbafa8b0
Kremilly <contact@kremilly.com>
2024-06-28 03:22:06 -0300

    refactor: update Wikipedia region formatting in Wikipedia addon

--- src/addons/wikipedia.rs
+++ src/addons/wikipedia.rs
@@ -26,7 +26,7 @@ impl Wikipedia {
 
     pub fn wikisource(url: &str) -> (String, String) {
         let wiki_name = UrlMisc::get_last_part(url);
-        let wikipedia_region = format!("{}.", Domains::get(url));
+        let wikipedia_region = format!("{}.", Domains::subdomain(url));
 
         let request_url = format!("{}{}", Uris::WIKISOURCE_API_REQUEST_PDF.to_string().replace(
             "en.", &wikipedia_region
