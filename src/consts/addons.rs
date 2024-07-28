53bb49a511eac3719b2f123c14bd0dc540ac41f0
Kremilly <contact@kremilly.com>
2024-07-28 10:34:29 -0300

    refactor: update URLs for default CSS style and extract covers plugin

--- src/consts/addons.rs
+++ src/consts/addons.rs
@@ -3,10 +3,10 @@ pub struct Addons;
 impl Addons {
 
     pub const DOWNLOAD_FILES_URI: &'static str = "https://raw.githubusercontent.com/Scibun/Scimon/main/";
-    pub const DEFAULT_CSS_STYLE: &'static str = "https://gist.githubusercontent.com/Kremilly/1e60287dfb89bce16bf160cc06afb2d0/raw/a380edebdc8e0c2598250039adac860b364328bd/github-flavored-md-style.css";
+    pub const DEFAULT_CSS_STYLE: &'static str = "https://addons.scibun.com/static/md-default.css";
     
     // Plugins
-    pub const EXTRACT_COVERS_PLUGIN: &'static str = "https://gist.githubusercontent.com/Kremilly/8ae07310c07093fe4208faf7b2b13b9d/raw/8ba216ea030f009da9557ab8a8b21684776c2c17/extract_covers.py";
+    pub const EXTRACT_COVERS_PLUGIN: &'static str = "https://raw.githubusercontent.com/Scibun/MonPlugins/main/extract_covers.py";
 
     // Scimon
     pub const SCIMON_API_REQUEST: &'static str = "http://localhost/Scimon/api/";
