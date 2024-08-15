5e2fa50d488959645504da7b13620ef469642dd1
Kremilly <contact@kremilly.com>
2024-08-15 13:49:46 -0300

    refactor: update regex_blocks.rs to fix regex patterns

--- src/regexp/regex_blocks.rs
+++ src/regexp/regex_blocks.rs
@@ -9,15 +9,16 @@ impl BlocksRegExp {
 
     pub const GET_PATH_VAR: &'static str = r#"(?i)path\s*=\s*"([^"]+)""#;
 
-    pub const GET_OPEN_VAR: &'static str = r#"(?i)open\s*=\s*"([^"]+)""#;
-
-    pub const GET_STYLE_VAR: &'static str = r#"(?i)style\s*=\s*"([^"]+)""#;
-
-    pub const GET_PRINT_VAR: &'static str = r#"(?i)print\s*=\s*"([^"]+)""#;
-
-    pub const GET_README_VAR: &'static str = r#"(?i)readme\s*=\s*"([^"]+)""#;
+    pub const GET_OPEN_VAR: &'static str = r#"(?i)open\s*"([^"]+)""#;
     
-    pub const GET_COVERS_VAR: &'static str = r#"(?i)covers\s*=\s*"([^"]+)""#;
-
-    pub const GET_COMPRESS_VAR: &'static str = r#"(?i)compress\s*=\s*"([^"]+)""#;    
+    pub const GET_STYLE_VAR: &'static str = r#"(?i)style\s*"([^"]+)""#;
+    
+    pub const GET_PRINT_VAR: &'static str = r#"(?i)print\s*"([^"]+)""#;
+    
+    pub const GET_README_VAR: &'static str = r#"(?i)readme\s*"([^"]+)""#;
+    
+    pub const GET_COVERS_VAR: &'static str = r#"(?i)covers\s*"([^"]+)""#;
+    
+    pub const GET_COMPRESS_VAR: &'static str = r#"(?i)compress\s*"([^"]+)""#;
+      
 }
