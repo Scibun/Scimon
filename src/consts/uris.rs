d0d47fdc71fac868e276e868e88d573017247ac8
Kremilly <contact@kremilly.com>
2024-08-16 14:57:23 -0300

    refactor: add support for gitlab repository name in markdown render

--- src/consts/uris.rs
+++ src/consts/uris.rs
@@ -2,10 +2,11 @@ pub struct Uris;
 
 impl Uris {
 
-    pub const PROVIDERS_DOMAINS: [&'static str; 3] = [
+    pub const PROVIDERS_DOMAINS: [&'static str; 4] = [
         "wikipedia.org",
         "wikisource.org",
         "raw.githubusercontent.com",
+        "gitlab.com"
     ];
 
     pub const WIKIPEDIA_API_REQUEST_PDF: &'static str = "https://en.wikipedia.org/api/rest_v1/page/pdf/";
--- src/utils/filename.rs
+++ src/utils/filename.rs
@@ -11,9 +11,19 @@ pub struct FileName;
 
 impl FileName {
 
+    fn split_url(url: &str) -> Vec<&str> {
+        url.split('/').collect()
+    }
+
     fn github_repo_name(url: &str) -> String {
-        let segments: Vec<&str> = url.split('/').collect();
+        let segments = Self::split_url(url);
         let repo_name = segments[segments.len() - 3];
+        repo_name.to_string()
+    }
+
+    fn gitlab_repo_name(url: &str) -> String {
+        let segments = Self::split_url(url);
+        let repo_name = segments[segments.len() - 5];
         repo_name.to_string()
     }
 
@@ -22,6 +32,7 @@ impl FileName {
 
         match domain.as_str() {
             domain if domain == Uris::PROVIDERS_DOMAINS[2] => Self::github_repo_name(url),
+            domain if domain == Uris::PROVIDERS_DOMAINS[3] => Self::gitlab_repo_name(url),
             _ => UrlMisc::get_last_part(url),
         }
     }
